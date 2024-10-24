10c603367e733478cfc5446c9e29e36ea4f0c01f
Oleksandr <lxbekas@gmail.com>
2024-10-24 20:33:57 +0300

    add basic folders, components and fonts.

--- moon_web/src/App.js
+++ moon_web/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-       
+      <Header/>
+       <h1>Moon</h1>
+       <Footer/>
     </div>
   );
 }
---
+++ moon_web/src/components/Header.js
@@ -0,0 +1,14 @@
+ 
+import React from 'react'
+
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Moon</span>
+        </div>
+
+    </header>
+  )
+}
